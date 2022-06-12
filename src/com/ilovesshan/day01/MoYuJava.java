11fd3dd880c9773e34f95e104324cac3da04ca10
wangyifeng <2665939276@qq.com>
2022-06-12 21:43:14 +0800

    什么也没干, 聊聊日常...

---
+++ src/com/ilovesshan/day01/MoYuJava.java
@@ -0,0 +1,15 @@
+package com.ilovesshan.day01;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2022/6/12
+ * @description:
+ */
+public class MoYuJava {
+    public static void main(String[] args) {
+        // 聊聊日常...
+        System.out.println("JavaSE 摸鱼第一天...");
+    }
+}
