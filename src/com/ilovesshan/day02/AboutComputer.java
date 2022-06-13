9b32e53a7d93babe27085da3d2b3ca2b2c0fc78c
wangyifeng <2665939276@qq.com>
2022-06-13 20:52:58 +0800

    了解了解关于计算机一些基本操作和了解java并且随手打印了HelloWorld~

---
+++ src/com/ilovesshan/day02/AboutComputer.java
@@ -0,0 +1,14 @@
+package com.ilovesshan.day02;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2022/6/13
+ * @description:
+ */
+public class AboutComputer {
+    public static void main(String[] args) {
+        System.out.println("Hello World~");
+    }
+}
