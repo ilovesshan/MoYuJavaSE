2382fd9f6d9c51267f35adad479615ee5c8d721f
wangyifeng <2665939276@qq.com>
2022-07-17 10:26:58 +0800

    数据局部性和缓存行, 以及所导致的:数据不可见性和数据乱序性

---
+++ src/com/ilovesshan/day20/CpuReadData.java
@@ -0,0 +1,17 @@
+package com.ilovesshan.day20;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author: ilovesshan
+ * @date: 2022/7/17
+ * @description:
+ */
+public class CpuReadData {
+    public static void main(String[] args) {
+        int[] nums = new int[10];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = i;
+        }
+    }
+}
